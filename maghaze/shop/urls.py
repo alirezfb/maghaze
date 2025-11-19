1e754e8e2954c7ae457d4b46efb88fbcdf0bc9c0
alirezfb <alirezfb@protonmail.com>
2025-11-19 13:23:32 +0330

    init

--- maghaze/shop/urls.py
+++ maghaze/shop/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path, include
-from .views import helloworld, about, login_user, logout_user
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', helloworld, name='home'),
-    path('about/', about, name='about'),
-    path('login/', login_user, name='login'),
-    path('logout/', logout_user, name='logout'),
+    path('', views.helloworld, name='home'),
+    path('about/', views.about, name='about'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('signup/', views.signup_user, name='signup'),
+    path('product/<int:pk>', views.product, name='product'),
+    path('category/<str:cat>', views.category, name='category'),
 ]
--- maghaze/shop/views.py
+++ maghaze/shop/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, HttpResponse, redirect
-from .models import Product
+from .models import Product, Category
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from .forms import SignUpForm
 
 def helloworld(request):
     all_products = Product.objects.all()
@@ -14,7 +18,6 @@ def login_user(request):
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
-
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
@@ -30,3 +33,35 @@ def logout_user(request):
     logout(request)
     messages.success(request, 'با موفقیت خارج شدید')
     return redirect('home')
+
+def signup_user(request):
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password1 = form.cleaned_data['password1']
+            user = authenticate(request, username=username, password=password1)
+            login(request, user)
+            messages.success(request, "اکانت شما با موفقیت ساخته شد")
+            return redirect("home")
+        else:
+            messages.success(request, "مشکلی در روند ثبت نام وجود داشت")
+            return redirect("signup")
+    else:
+        return render(request, 'signup.html', {'form':form})
+
+def product(request,pk):
+    product_object = Product.objects.get(id=pk)
+    return render(request, 'product.html', {'product': product_object})
+
+def category(request,cat):
+    cat = cat.replace("-"," ")
+    try:
+        category = Category.objects.get(name=cat)
+        products = Product.objects.filter(category=category)
+        return render(request, 'category.html', {'products': products, "category": category})
+    except:
+        messages.success(request, "دسته بندی وجود ندارد")
+        return redirect("home")
