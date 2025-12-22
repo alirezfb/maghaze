29b538c16594670a4463213b873567aecff1c623
alirezfb <alirezfb@protonmail.com>
2025-12-22 14:56:28 +0330

    init

--- maghaze/payment/forms.py
+++ maghaze/payment/forms.py
@@ -48,15 +48,13 @@ class ShippingForm(forms.ModelForm):
         fields = [
             'shipping_full_name',
             'shipping_email',
-            'shipping_phone',
             'shipping_address1',
             'shipping_address2',
             'shipping_city',
             'shipping_city',
             'shipping_state',
             'shipping_zipcode',
-            'shipping_country',
-            'shipping_old_cart'
+            'shipping_country'
         ]
 
         exclude = ['user',]
--- maghaze/payment/models.py
+++ maghaze/payment/models.py
@@ -1,22 +1,42 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from shop.models import Product
 class ShippingAddress(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     shipping_full_name = models.CharField(max_length=250)
-    shipping_email = models.CharField(max_length=300)
-    shipping_phone = models.CharField(max_length=25, blank=True)
+    shipping_email = models.CharField(max_length=250)
     shipping_address1 = models.CharField(max_length=250, blank=True)
     shipping_address2 = models.CharField(max_length=250, blank=True, null=True)
     shipping_city = models.CharField(max_length=25, blank=True)
-    shipping_state = models.CharField(max_length=25, blank=True)
-    shipping_zipcode = models.CharField(max_length=25, blank=True)
+    shipping_state = models.CharField(max_length=25, blank=True, null=True)
+    shipping_zipcode = models.CharField(max_length=25, blank=True, null=True)
     shipping_country = models.CharField(max_length=25, default='IRAN')
-    shipping_old_cart = models.CharField(max_length=200, blank=True, null=True)
 
     class Meta:
-        verbose_name_plural = 'Shipping Addresses'
+        verbose_name_plural = 'Shipping Address'
 
 
     def __str__(self):
-        return f'Shipping Address From {self.full_name}'
+        return f'Shipping Address - {str(self.shipping_full_name)}'
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    full_name = models.CharField(max_length=250)
+    email = models.EmailField(max_length=300)
+    shipping_address = models.TextField(max_length=150000)
+    amount_paid = models.DecimalField(decimal_places=0, max_digits=12)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Order - {str(self.id)}'
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True)
+    products = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.DecimalField(decimal_places=0, max_digits=12)
+
+    def __str__(self):
+        return f'Order Item - {str(self.id)}'
